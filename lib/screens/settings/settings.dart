8a95084cb1a8300a45086dae67ed60f0d8cd58a8
Adarsh V S <adarsh@zdistancelab.com>
2023-05-28 23:46:26 +0530

    updated settings skeleton

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -7,63 +7,65 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: CircleAvatar(
-              radius: 60,
-              backgroundColor: Colors.blue,
-              child: Icon(
-                Icons.person,
-                size: 80,
-                color: Colors.white,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CircleAvatar(
+                radius: 60,
+                backgroundColor: Colors.blue,
+                child: Icon(
+                  Icons.person,
+                  size: 80,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ListTile(
-                    leading: Icon(Icons.account_circle),
-                    title: Text('Edit Profile'),
-                    onTap: () {
-                      // Handle Edit Profile tap
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.notifications),
-                    title: Text('Notifications'),
-                    onTap: () {
-                      // Handle Notifications tap
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.privacy_tip),
-                    title: Text('Privacy'),
-                    onTap: () {
-                      // Handle Privacy tap
-                    },
-                  ),
-                  // Add more options as needed
-                ],
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.account_circle),
+                      title: Text('Edit Profile'),
+                      onTap: () {
+                        // Handle Edit Profile tap
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.notifications),
+                      title: Text('Notifications'),
+                      onTap: () {
+                        // Handle Notifications tap
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.privacy_tip),
+                      title: Text('Privacy'),
+                      onTap: () {
+                        // Handle Privacy tap
+                      },
+                    ),
+                    // Add more options as needed
+                  ],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Handle Logout button tap
-              },
-              child: Text('Logout'),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle Logout button tap
+                },
+                child: Text('Logout'),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
